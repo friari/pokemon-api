58e67d116337a4ccb666cad218a226474cfb2acf
Ari F <afriedgut1@gmail.com>
2021-08-28 18:05:15 +1000

    99% there - need to work on all pokemon request

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-require('express');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const fetch = require('node-fetch');
+
+
+let pokemonAll = [];
+
+fetch(
+  'https://pokeapi.co/api/v2/pokemon?limit=100',
+  { method: 'GET' }
+)
+.then(response => response.json())
+.then(json => {
+  json.results.forEach((result, i) => {
+    pokemonAll.push({ id: (i+1), name: result.name });
+  });
+})
+.catch(e => console.log(e));
+
+app.get('/pokemon', (req, res) => {
+  console.log(req);
+  res.send(pokemonAll);
+});
+
+app.get('/pokemon/:id', (req, res) => {
+  const pokemon = pokemonAll.find(p => p.id === parseInt(req.params.id));
+  res.send(pokemon);
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
